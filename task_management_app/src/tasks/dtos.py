4eaf092d75a48e83a87d9d27b75691e0b7fc1cd8
Shubhajit <shubhajitm1996@gmail.com>
2026-04-13 19:19:05 +0530

    Implemented Response Model TaskResponseSchema

--- task_management_app/src/tasks/dtos.py
+++ task_management_app/src/tasks/dtos.py
@@ -5,3 +5,9 @@ class Taskschema(BaseModel):
      title :str
      description : str
      is_completed :bool = False
+
+class TaskResponseSchema(BaseModel):
+     id: int
+     title :str
+     description : str
+     is_completed :bool
