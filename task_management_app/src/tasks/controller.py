31068cc5d6b604caf915a3436aeba79f8f6ed280
Shubhajit <shubhajitm1996@gmail.com>
2026-04-14 21:21:22 +0530

    added new column in the tasks table in DB using alembic migrations

--- task_management_app/src/tasks/controller.py
+++ task_management_app/src/tasks/controller.py
@@ -2,12 +2,14 @@ from src.tasks.dtos import Taskschema
 from sqlalchemy.orm import Session
 from src.tasks.models import TaskModel
 from fastapi import HTTPException
+from src.user.models import UserModel
 
-def create_task(body:Taskschema , db:Session):
+def create_task(body:Taskschema , db:Session , user:UserModel):
     data = body.model_dump()
     new_task = TaskModel(title= data["title"] , 
                          description = data["description"],
-                         is_completed = data["is_completed"]
+                         is_completed = data["is_completed"],
+                         user_id = user.id
                          )
     
     db.add(new_task)
